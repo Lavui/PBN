e0df68589f661f4419ea74520463043078dd9f49
hafidk <hafidstor@gmail.com>
2017-04-20 10:49:18 +0200

    Lamp.c acabat

--- p5/lamp.c
+++ p5/lamp.c
@@ -17,21 +17,70 @@ void lamp_init(
 
 
 void lamp_on(lamp_t l, color_t c){
-  l->c
+  switch (c){
+  case Green:
+    pin_w(l.green,true);
+    break;
+  case Red:
+    pin_w(l.red,true);
+    break;
+  case Yellow:
+    pin_w(l.yellow,true);
+    break;
+  default:
+    break; //Catch all condition, no es complira.
+  }
 }
 
 
-void lamp_off(lamp_t l, color_t c);{
 
+void lamp_off(lamp_t l, color_t c);{
+  switch (c){
+  case Green:
+    pin_w(l.green,false);
+    break;
+  case Red:
+    pin_w(l.red,false);
+    break;
+  case Yellow:
+    pin_w(l.yellow,false);
+    break;
+  default:
+    break; //Catch all condition, no es complira.
+  }
 }
 
 
 void lamp_toggle(lamp_t l, color_t c);{
-
+  switch (c){
+  case Green:
+    pin_toggle(l.green);
+    break;
+  case Red:
+    pin_toggle(l.red);
+    break;
+  case Yellow:
+    pin_toggle(l.yellow);
+    break;
+  default:
+    break; //Catch all condition, no es complira.
+  }
 }
 
 
 bool lamp_is_on(lamp_t l, color_t c);{
-
+  switch (c){
+  case Green:
+    bit_is_set(l.green.port,l.green.pin);
+    break;
+  case Red:
+    pin_toggle(l.red.port,l.red.pin);
+    break;
+  case Yellow:
+    pin_toggle(l.yellow.port,l.yellow.pin);
+    break;
+  default:
+    break; //Catch all condition, no es complira.
+  }
 }
 
